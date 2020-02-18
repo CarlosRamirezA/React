5f318cff788b7f405658e01e3d4e19edb3c5b931
CarlosRamirezA <carlosramirez2507@hotmail.com>
2020-02-17 21:02:26 -0500

    route propiedades y url prametros

--- src/react_router.js
+++ src/react_router.js
@@ -2,8 +2,43 @@ import React from 'react';
 import { BrowserRouter, Route, Link} from 'react-router-dom';
 
 const Home = () => <div>Home</div>
-const About = () => <div>About</div>
-const User = ({match}) => <div>{match.url}</div>
+const About = (props) => {
+    console.log(props.match)
+    console.log(props.location)
+    console.log(props.history)
+
+    return(
+        <div>
+            About
+        </div>
+    )
+
+} 
+const User = ({match}) => {
+    return(
+        <div>{match.params.id}</div>
+    )
+}
+
+const Info = ({match}) => {
+    return (
+      <div>
+        <Route path="{match.url + '/phone'}" 
+                render={(props) => <div>Phone: 123 456 7890</div>}/> 
+        <Route path="{match.    url + '/email'}" 
+                render={(props) => <div>Email: email@email.com</div>}/> 
+        <Route path="{match.url + '/address'}" 
+                render={(props) => <div>Address: 123 Address</div>}/>
+      </div>
+  )}
+  
+  /*
+  MATCH (parametroz, isexact,ruta, url)
+  LOCATION ((ruta,search,hash,estado))
+  HISTORY (longitu,accion,ubicacion, metodos(push,pop,replace))
+   */
+    
+
 
 class React_Router extends React.Component{
     render(){
@@ -19,13 +54,17 @@ class React_Router extends React.Component{
                     <li>
                         <Link to="/users">Users</Link>
                     </li>
+                    <li>
+                        <Link to="/info">Info</Link>
+                    </li>
                 </ul>
                 
                 <hr/>
 
                 <Route exact path="/" component={Home}/>
-                <Route strict path="/about/"render= {() => <div>About</div> }/>
-                <Route path="/users" children={() => <div>Rendered</div>}/>
+                <Route path="/about"component={About}/>
+                <Route path="/userId/:id" component={User}/>
+                <Route path="/info" component = {Info} />
             </BrowserRouter>
             
         )
