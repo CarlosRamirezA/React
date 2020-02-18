e4601ae9c1764d136ed241b85ba3705235c4a566
CarlosRamirezA <carlosramirez2507@hotmail.com>
2020-02-18 09:19:36 -0500

    Router Switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React  from 'react';
 import React_Router from './react_router';
 import Linked from './router_link';
+import SwitchRoute from './switch'
 
 class App extends React.Component {
 
   render(){
     return (
     <div>
-      <Linked/>
+      <SwitchRoute/>
     </div>
   )
  };
---
+++ src/switch.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const AllUsers = () => {
+  return (
+    <div>
+      Show All Users:
+      <ul>
+        <li>abc</li>
+        <li>def</li>
+        <li>ghi</li>
+      </ul>
+    </div>
+  );
+};
+const Users = ({ match }) => <div>{match.params.id}</div>;
+const DefaultRoute = () => <div>Default Route</div>;
+
+class SwitchRoute extends React.Component{
+  
+  render(){
+      return(
+          <BrowserRouter>
+            <div>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users/abc">Users: abc</Link>
+                </li>
+                <li>
+                  <Link to="/users/allUsers">Show All Users</Link>
+                </li>
+              </ul>
+              <hr />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/users/allUsers" component={AllUsers} />
+                <Route path="/users/:id" component={Users} />
+                <Route component={DefaultRoute} />
+              </Switch>
+            </div>
+          </BrowserRouter>
+      )
+  }
+}
+
+export default SwitchRoute;
+
